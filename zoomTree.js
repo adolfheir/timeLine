379035ad5ae46a9625919cbba4ac8e5ffd730ef2
wb-cyh578854 <wb-cyh578854@alibaba-inc.com>
2019-09-04 10:51:51 +0800

    refactor:提前zoomTree配置项

---
+++ zoomTree.js
@@ -0,0 +1,607 @@
+window.__zoomTree__ = [
+{
+    unit: "d",
+    width: 35000,
+    level: 1,
+    label: "30 minutes"
+},
+{
+    unit: "d",
+    width: 17600,
+    level: 2,
+    label: "1 hour"
+},
+{
+    unit: "d",
+    width: 8800,
+    level: 3,
+    label: "2 hours"
+},
+{
+    unit: "d",
+    width: 4400,
+    level: 4,
+    label: "5 hours"
+},
+{
+    unit: "d",
+    width: 2200,
+    level: 5,
+    label: "10 hours"
+},
+{
+    unit: "d",
+    width: 1100,
+    level: 6,
+    label: "1 DAY"
+},
+{
+    unit: "d",
+    width: 550,
+    level: 7,
+    label: "40 hours"
+},
+{
+    unit: "d",
+    width: 432,
+    level: 8,
+    label: "2 days"
+},
+{
+    unit: "d",
+    width: 343,
+    level: 9,
+    label: "2.5 days"
+},
+{
+    unit: "d",
+    width: 272,
+    level: 10,
+    label: "3 days"
+},
+{
+    unit: "d",
+    width: 216,
+    level: 11,
+    label: "4 days"
+},
+{
+    unit: "d",
+    width: 171,
+    level: 12,
+    label: "5 days"
+},
+{
+    unit: "d",
+    width: 136,
+    level: 13,
+    label: "1 WEEK"
+},
+{
+    unit: "d",
+    width: 108,
+    level: 14,
+    label: "8 days"
+},
+/* 108 * 30 = equiv to a 3240 month */
+{
+    unit: "M",
+    width: 2509,
+    level: 15,
+    label: "10 days"
+},
+{
+    unit: "M",
+    width: 1945,
+    level: 16,
+    label: "2 WEEKS"
+},
+{
+    unit: "M",
+    width: 1508,
+    level: 17,
+    label: "18 days"
+},
+{
+    unit: "M",
+    width: 1169,
+    level: 18,
+    label: "3 weeks"
+},
+{
+    unit: "M",
+    width: 913,
+    level: 19,
+    label: "1 MONTH"
+},
+{
+    unit: "M",
+    width: 719,
+    level: 20,
+    label: "5 weeks"
+},
+{
+    unit: "M",
+    width: 566,
+    level: 21,
+    label: "6 weeks"
+},
+{
+    unit: "M",
+    width: 453,
+    level: 22,
+    label: "2 MONTHS"
+},
+
+{
+    unit: "M",
+    width: 362,
+    level: 23,
+    label: "10 weeks"
+},
+{
+    unit: "M",
+    width: 290,
+    level: 24,
+    label: "3 MONTHS"
+},
+{
+    unit: "M",
+    width: 232,
+    level: 25,
+    label: "4 months"
+},
+{
+    unit: "M",
+    width: 186,
+    level: 26,
+    label: "5 months"
+},
+{
+    unit: "M",
+    width: 148,
+    level: 27,
+    label: "6 MONTHS"
+},
+{
+    unit: "M",
+    width: 119,
+    level: 28,
+    label: "7 months"
+},
+{
+    unit: "M",
+    width: 95,
+    level: 29,
+    label: "9 months"
+},
+{
+    unit: "M",
+    width: 76,
+    level: 30,
+    label: "1 YEAR"
+},
+/* 76 * 12 = equiv to a 912 year */
+{
+    unit: "y",
+    width: 723,
+    level: 31,
+    label: "15 months"
+},
+{
+    unit: "y",
+    width: 573,
+    level: 32,
+    label: "18 months"
+},
+{
+    unit: "y",
+    width: 455,
+    level: 33,
+    label: "2 YEARS"
+},
+{
+    unit: "y",
+    width: 361,
+    level: 34,
+    label: "2.5 years"
+},
+{
+    unit: "y",
+    width: 286,
+    level: 35,
+    label: "3 years"
+},
+{
+    unit: "y",
+    width: 227,
+    level: 36,
+    label: "4 years"
+},
+{
+    unit: "y",
+    width: 179,
+    level: 37,
+    label: "5 years"
+},
+{
+    unit: "y",
+    width: 142,
+    level: 38,
+    label: "6 years"
+},
+{
+    unit: "y",
+    width: 113,
+    level: 39,
+    label: "8 years"
+},
+{
+    unit: "y",
+    width: 89,
+    level: 40,
+    label: "10 years"
+},
+{
+    unit: "de",
+    width: 705,
+    level: 41,
+    label: "13 years"
+},
+{
+    unit: "de",
+    width: 559,
+    level: 42,
+    label: "16 years"
+},
+{
+    unit: "de",
+    width: 443,
+    level: 43,
+    label: "20 years"
+},
+
+{
+    unit: "de",
+    width: 302,
+    level: 44,
+    label: "25 years"
+},
+{
+    unit: "de",
+    width: 240,
+    level: 45,
+    label: "30 years"
+},
+{
+    unit: "de",
+    width: 190,
+    level: 46,
+    label: "40 years"
+},
+{
+    unit: "de",
+    width: 150,
+    level: 47,
+    label: "50 years"
+},
+{
+    unit: "de",
+    width: 120,
+    level: 48,
+    label: "65 years"
+},
+{
+    unit: "de",
+    width: 95,
+    level: 49,
+    label: "80 years"
+},
+{
+    unit: "de",
+    width: 76,
+    level: 50,
+    label: "100 YEARS"
+},
+{
+    unit: "ce",
+    width: 600,
+    level: 51,
+    label: "130 years"
+},
+{
+    unit: "ce",
+    width: 480,
+    level: 52,
+    label: "160 years"
+},
+{
+    unit: "ce",
+    width: 381,
+    level: 53,
+    label: "200 YEARS"
+},
+{
+    unit: "ce",
+    width: 302,
+    level: 54,
+    label: "250 years"
+},
+{
+    unit: "ce",
+    width: 240,
+    level: 55,
+    label: "300 years"
+},
+{
+    unit: "ce",
+    width: 190,
+    level: 56,
+    label: "400 years"
+},
+{
+    unit: "ce",
+    width: 150,
+    level: 57,
+    label: "500 YEARS"
+},
+{
+    unit: "ce",
+    width: 120,
+    level: 58,
+    label: "600 years"
+},
+{
+    unit: "ce",
+    width: 95,
+    level: 59,
+    label: "1000 YEARS"
+},
+{
+    unit: "ce",
+    width: 76,
+    level: 60,
+    label: "1100 years"
+},
+{
+    unit: "thou",
+    width: 603,
+    level: 61,
+    label: "1500 years"
+},
+
+{
+    unit: "thou",
+    width: 478,
+    level: 62,
+    label: "2000 years"
+},
+{
+    unit: "thou",
+    width: 379,
+    level: 63,
+    label: "2500 years"
+},
+{
+    unit: "thou",
+    width: 301,
+    level: 64,
+    label: "3000 years"
+},
+{
+    unit: "thou",
+    width: 239,
+    level: 65,
+    label: "4000 years"
+},
+{
+    unit: "thou",
+    width: 190,
+    level: 66,
+    label: "5000 YEARS"
+},
+{
+    unit: "thou",
+    width: 150,
+    level: 67,
+    label: "6000 years"
+},
+{
+    unit: "thou",
+    width: 120,
+    level: 68,
+    label: "7500 years"
+},
+{
+    unit: "thou",
+    width: 95,
+    level: 69,
+    label: "10,000 YEARS"
+},
+{
+    unit: "thou",
+    width: 76,
+    level: 70,
+    label: "12,000 years"
+},
+{
+    unit: "tenthou",
+    width: 603,
+    level: 71,
+    label: "15,000 years"
+},
+{
+    unit: "tenthou",
+    width: 358,
+    level: 72,
+    label: "25,000 years"
+},
+{
+    unit: "tenthou",
+    width: 213,
+    level: 73,
+    label: "40,000 years"
+},
+{
+    unit: "tenthou",
+    width: 126,
+    level: 74,
+    label: "70,000 years"
+},
+{
+    unit: "tenthou",
+    width: 76,
+    level: 75,
+    label: "100,000 YEARS"
+},
+{
+    unit: "hundredthou",
+    width: 603,
+    level: 76,
+    label: "150,000 years"
+},
+{
+    unit: "hundredthou",
+    width: 358,
+    level: 77,
+    label: "250,000 years"
+},
+{
+    unit: "hundredthou",
+    width: 213,
+    level: 78,
+    label: "400,000 years"
+},
+{
+    unit: "hundredthou",
+    width: 126,
+    level: 79,
+    label: "700,000 years"
+},
+{
+    unit: "hundredthou",
+    width: 76,
+    level: 80,
+    label: "1 million years"
+},
+{
+    unit: "mill",
+    width: 603,
+    level: 81,
+    label: "1.5 million years"
+},
+{
+    unit: "mill",
+    width: 358,
+    level: 82,
+    label: "3 million years"
+},
+{
+    unit: "mill",
+    width: 213,
+    level: 83,
+    label: "4 million years"
+},
+{
+    unit: "mill",
+    width: 126,
+    level: 84,
+    label: "6 million years"
+},
+{
+    unit: "mill",
+    width: 76,
+    level: 85,
+    label: "10 million years"
+},
+{
+    unit: "tenmill",
+    width: 603,
+    level: 86,
+    label: "15 million years"
+},
+{
+    unit: "tenmill",
+    width: 358,
+    level: 87,
+    label: "25 million years"
+},
+{
+    unit: "tenmill",
+    width: 213,
+    level: 88,
+    label: "40 million years"
+},
+{
+    unit: "tenmill",
+    width: 126,
+    level: 89,
+    label: "70 million years"
+},
+{
+    unit: "tenmill",
+    width: 76,
+    level: 90,
+    label: "100 million years"
+},
+{
+    unit: "hundredmill",
+    width: 603,
+    level: 91,
+    label: "120 million years"
+},
+{
+    unit: "hundredmill",
+    width: 358,
+    level: 92,
+    label: "200 million years"
+},
+{
+    unit: "hundredmill",
+    width: 213,
+    level: 93,
+    label: "300 million years"
+},
+{
+    unit: "hundredmill",
+    width: 126,
+    level: 94,
+    label: "500 million years"
+},
+{
+    unit: "hundredmill",
+    width: 76,
+    level: 95,
+    label: "1 billion years"
+},
+{
+    unit: "bill",
+    width: 603,
+    level: 96,
+    label: "15 million years"
+},
+{
+    unit: "bill",
+    width: 358,
+    level: 97,
+    label: "30 million years"
+},
+{
+    unit: "bill",
+    width: 213,
+    level: 98,
+    label: "50 million years"
+},
+{
+    unit: "bill",
+    width: 126,
+    level: 99,
+    label: "80 million years"
+},
+{
+    unit: "bill",
+    width: 76,
+    level: 100,
+    label: "100 billion years"
+}
+];
